a4c5ef4136e4f57e7cc21a57e52a9a42575e6aa2
Enes Kuzucu <31979644+karaposu@users.noreply.github.com>
2022-01-31 10:50:16 -0800

    Update p253.h

--- p253.h
+++ p253.h
@@ -1,13 +1,44 @@
 
-#define BNO055_WR_FUNC_PTR int8_t (*bus_write)(uint8_t, uint8_t, uint8_t *, uint8_t)
-
-#define BNO055_RD_FUNC_PTR int8_t (*bus_read)(uint8_t, uint8_t, uint8_t *, uint8_t)
-
-
-
 // 253***sign_byte_array****8bit_data_array****16bit_data_array***fraction_data_array8bit***fraction_data_array16bit***fraction_index8bit***fraction_index16bit
 
+//-This protocol allow us to send a package which contains such data =  [255  -125.65 100 200 -10500 62000 -32125.16 ]
+//-As you see this package contains different 8bit data, fraction data, 16 bit data all together. 
+//-And if you need to add another 8 bit data in your application you dont have to worry about rearranging the delimeter logic in the receiving part.
+//-All you need to change is initialization key in receiving side
+//-initialization key is a list of numbers which encodes the type and order  and meaning of each 8bit data in a package
+//-Once initialization key is known receiving side will know which value in package correcponds to what. 
+//-Packages send via COBS
 
+
+//First thing we protocol does is to seperate fraction parts. And we save the fractions in a list as well as which fraction belongs to which data
+//Second process is to extract sign information and save them in another list as well as which sign belongs to which data
+//Third step is to create a package with order of   *sign_byte_array  *8bit_data_array *16bit_data_array *fraction_data_array *fraction_index
+
+
+//  p253_init3  initializes with using these values:
+	
+    //n_of8bit = number of 8bit values in package
+	//n_of16bit = number of 16bit values in package
+	//n_of_f = how many 8bit values will allowed to have fraction parts  
+	//n_of_f16 = how many 16bit values will allowed to have fraction parts  
+	//frac_indx_arry = an array which shows the order of fraction enabled 8bit values in the package 
+	//( lets say we have three 8bit values and only the last one have a fraction part.In this case frac_indx_arry= 0001  ) 
+	//frac_indx_arry = an array which shows the order of fraction enabled 16bit values in the package 
+	
+
+
+
+//*p253, uint8_t n_of8bit ,uint8_t n_of16bit,   uint8_t n_of_f,   uint8_t n_of_f16, uint8_t *frac_indx_arry, uint8_t *frac_indx_arry16){
+  
+
+
+//	extract_fraction
+//	p253_make_prepackage_distill_8_16
+//	p253_make_prepackage_signs_8_16
+//	p253_package
+//	zip2byte_array2
+//	p253_make_prepackage_create_fraction_index_byte8
+//	p253_make_prepackage_create_fraction_index_byte16
 
 typedef struct {
     int8_t  total_space;
@@ -29,15 +60,14 @@ typedef struct {
 	int8_t  frac_indx_arry16[ 30]; 
 	int8_t  fraction_byte_array_lenght16bit;
 	
-	
-	BNO055_WR_FUNC_PTR;/**< bus write function pointer */
-	BNO055_RD_FUNC_PTR;/**<bus read function pointer */
+
   
 }p253_t;
 
 
+// 253***sign_byte_array****8bit_data_array****16bit_data_array***fraction_data_array***fraction_index
 uint8_t p253_init3(p253_t *p253, uint8_t n_of8bit ,uint8_t n_of16bit,   uint8_t n_of_f,   uint8_t n_of_f16, uint8_t *frac_indx_arry, uint8_t *frac_indx_arry16){
-  // 253***sign_byte_array****8bit_data_array****16bit_data_array***fraction_data_array***fraction_index
+  
 
    uint8_t a=0;  uint8_t b=0;  uint8_t c=0;  uint8_t d=0;  uint8_t e=0;uint8_t f=0;  uint8_t g=0;
    a= n_of8bit     ;      b= n_of16bit     ;   c= n_of_f    ;     d=   ((b+a-1)/8) +1 ;  e=   ((c-1)/8) +1 ;        f= n_of_f16;        g=   ((f-1)/8) +1 ;    
@@ -75,24 +105,16 @@ int extract_fraction( float data_f ,uint8_t precision){
 int8_t p253_make_prepackage_distill_8_16(p253_t *p253,  float *values_8bit ,uint8_t *a ,uint8_t *b, float *values_16bit ,uint8_t *c ,uint16_t *d){  
 
 
-       uint8_t n0f16 = p253->n_of16bit; uint8_t  n0f8  =p253->n_of8bit ;uint8_t  frac_indx_arry[n0f8]  ; uint8_t  frac_indx_arry16[n0f16]  ;
-        float temp;  int8_t  sign=1;  int8_t  memory=0;
+    uint8_t n0f16 = p253->n_of16bit; uint8_t  n0f8  =p253->n_of8bit ;uint8_t  frac_indx_arry[n0f8]  ; uint8_t  frac_indx_arry16[n0f16]  ;
+    float temp;  int8_t  sign=1;  int8_t  memory=0;
 
- for(int i=0;i<n0f8;i++){ frac_indx_arry[i]=     p253->frac_indx_arry[ i];}//Serial.print( "frac_indx_arry_inside_distill :");  Serial.print( "  ");   for(int i=0;i< n0f8 ;i++){Serial.print(  frac_indx_arry[i] );  Serial.print( " ");delay(10);}  ;Serial.println();Serial.println();
- 
- for(int i=0;i<n0f16;i++){frac_indx_arry16[i]=     p253->frac_indx_arry16[ i];}//Serial.print( "frac_indx_arry16_inside_distill :");  Serial.print( "  ");   for(int i=0;i< n0f16 ;i++){Serial.print(  frac_indx_arry16[i] );  Serial.print( " ");delay(10);}  ;Serial.println();Serial.println();
-      
-
-                                                            
-     for(int i=0;i<n0f8;i++){
+    for(int i=0;i<n0f8;i++){ frac_indx_arry[i]=     p253->frac_indx_arry[ i];}//Serial.print( "frac_indx_arry_inside_distill :");  Serial.print( "  ");   for(int i=0;i< n0f8 ;i++){Serial.print(  frac_indx_arry[i] );  Serial.print( " ");delay(10);}  ;Serial.println();Serial.println();
+    for(int i=0;i<n0f16;i++){frac_indx_arry16[i]=     p253->frac_indx_arry16[ i];}//Serial.print( "frac_indx_arry16_inside_distill :");  Serial.print( "  ");   for(int i=0;i< n0f16 ;i++){Serial.print(  frac_indx_arry16[i] );  Serial.print( " ");delay(10);}  ;Serial.println();Serial.println();
+    for(int i=0;i<n0f8;i++){
        sign=1; 
        temp  =    *(values_8bit+i);
-  
-       if(frac_indx_arry[i]==1){
-       *(a + memory)  =    extract_fraction(temp ,2);memory++;
-     
-       }
-        *(b + i)  =        floor (temp) * sign;
+       if(frac_indx_arry[i]==1){ *(a + memory)  =    extract_fraction(temp ,2);memory++; }
+       *(b + i)  =        floor (temp) * sign;
 
     }
     temp=0;memory=0;
@@ -211,8 +233,8 @@ uint8_t p253_package( p253_t *p253,uint8_t *values_8bit ,uint16_t *values_16bit,
          for(int i=0;i<n0f8;i++){   raw_packet_8bit[i+sbai]=*(values_8bit+i);}
          
          for(int i=0  ;i< n0f16;    i++){  
-          raw_packet_8bit[(i*2+sbai+n0f8)+0]= (  *(values_16bit+i) & 0x00FF    ); // 0 için 12 ye  yazdýrýr.   1 için 14 e yazdýrýr 
-          raw_packet_8bit[(i*2+sbai+n0f8)+1]=  ( *(values_16bit+i) )>> 8 ;        //  0  için 13 e yazdýrýr.    1 için 15 e yazdýrýr.
+          raw_packet_8bit[(i*2+sbai+n0f8)+0]= (  *(values_16bit+i) & 0x00FF    ); // 0 iï¿½in 12 ye  yazdï¿½rï¿½r.   1 iï¿½in 14 e yazdï¿½rï¿½r 
+          raw_packet_8bit[(i*2+sbai+n0f8)+1]=  ( *(values_16bit+i) )>> 8 ;        //  0  iï¿½in 13 e yazdï¿½rï¿½r.    1 iï¿½in 15 e yazdï¿½rï¿½r.
          }
          for(int i=0  ;i< noff;    i++){ raw_packet_8bit[sbai+n0f8+n0f16*2+i]=*(values_fraction+i); }
           for(int i=0  ;i< noff16;    i++){ raw_packet_8bit[sbai+n0f8+n0f16*2+noff+i]=*(values16bit_fraction+i); }
@@ -398,93 +420,7 @@ uint8_t p253_make_prepackage_create_fraction_index_byte16(p253_t *p253, int8_t *
 
   
 	
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
-	 
 	
-
-
-	
-	
-	
-	
-
-
-
-
-/*  \Brief: The API is used as I2C bus write
- *  \Return : Status of the I2C write
- *  \param dev_addr : The device address of the sensor
- *  \param reg_addr : Address of the first register,
- *   will data is going to be written
- *  \param reg_data : It is a value hold in the array,
- *    will be used for write the value into the register
- *  \param cnt : The no of byte of data to be write
- */
- 
- 
- 
-// s8 BNO055_I2C_bus_write(u8 dev_addr, u8 reg_addr, u8 *reg_data, u8 cnt)
-// {
-  // s32 BNO055_iERROR = BNO055_INIT_VALUE;
-  // u8 array[I2C_BUFFER_LEN];
-  // u8 stringpos = BNO055_INIT_VALUE;
-
-  // array[BNO055_INIT_VALUE] = reg_addr;
-  // for (stringpos = BNO055_INIT_VALUE; stringpos < cnt; stringpos++)
-    // array[stringpos + BNO055_I2C_BUS_WRITE_ARRAY_INDEX] =
-      // *(reg_data + stringpos);
-  // }
-
-
-  // s8 I2C_routine(void)
-// {
-  // bno055.bus_write = BNO055_I2C_bus_write;
-  // bno055.bus_read = BNO055_I2C_bus_read;
-  // bno055.delay_msec = BNO055_delay_msek;
-  // bno055.dev_addr = BNO055_I2C_ADDR1;
-
-  // return BNO055_INIT_VALUE;
-// }
-
-
-
-
-
-
-
-// s8  hal_i2c_write( u8 dev_addr,u8  reg_addr,u8  *data,   u8  length) {
-  
-
-  // u8 buffer[8];
-  // u8 i =0;
-  
-  // buffer[0] =reg_addr;  
-  // for( i =0;i<length;i++)  // 0 için buffer_global(1) e data_gl[0]  i atiyor.    // 1 için 
-   // {
-     // buffer[i+1]=  (*data+i)  ;
-   // }
-                        
-  
-  
-    // if  (HAL_I2C_Master_Transmit(&hi2c1,dev_addr,buffer,length+1,100)==HAL_OK  ){return 0;}
-   // else {return -1;}
-// }
-
 
 
 
